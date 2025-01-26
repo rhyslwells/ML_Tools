7e13d22125bc86948427e3943d7be60e0f013a7d
Rhys Wells <rhyslwells@outlook.com>
2025-01-26 17:16:03 +0000

    Removed 0_Exploration folder

---
+++ .history/0_Exploration/Utilities/Vectorisation_20250126171548.py
@@ -0,0 +1,42 @@
+# Time comparision of vectorisation
+
+import numpy as np    # it is an unofficial standard to use np for numpy
+import time
+
+def my_dot(a, b): 
+    """
+   Compute the dot product of two vectors
+ 
+    Args:
+      a (ndarray (n,)):  input vector 
+      b (ndarray (n,)):  input vector with same dimension as a
+    
+    Returns:
+      x (scalar): 
+    """
+    x=0
+    for i in range(a.shape[0]):
+        x = x + a[i] * b[i]
+    return x
+
+np.random.seed(1)
+a = np.random.rand(10000000)  # very large arrays
+b = np.random.rand(10000000)
+
+tic = time.time()  # capture start time
+c = np.dot(a, b)
+toc = time.time()  # capture end time
+
+print(f"np.dot(a, b) =  {c:.4f}")
+print(f"Vectorized version duration: {1000*(toc-tic):.4f} ms ")
+
+tic = time.time()  # capture start time
+c = my_dot(a,b)
+toc = time.time()  # capture end time
+
+print(f"my_dot(a, b) =  {c:.4f}")
+print(f"loop version duration: {1000*(toc-tic):.4f} ms ")
+
+
+
+
---
+++ Optimisation/Gradient_Descent.py
@@ -0,0 +1,26 @@
+# Notes: Gradient Descent:------------------------
+
+from sklearn.linear_model import SGDRegressor
+
+'''
+Load data
+Use StandardScaler to normalize the data
+Make model
+Make Predictions
+'''
+
+# Scikit-learn has a gradient descent regression model [sklearn.linear_model.SGDRegressor](https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.SGDRegressor.html#examples-using-sklearn-linear-model-sgdregressor).                                                                     
+sgdr = SGDRegressor(max_iter=1000)
+sgdr.fit(X_norm, y_train)
+print(sgdr)
+print(f"number of iterations completed: {sgdr.n_iter_}, number of weight updates: {sgdr.t_}")
+
+# plot predictions and targets vs original features    
+fig,ax=plt.subplots(1,4,figsize=(12,3),sharey=True)
+for i in range(len(ax)):
+    ax[i].scatter(X_train[:,i],y_train, label = 'target')
+    ax[i].set_xlabel(X_features[i])
+    ax[i].scatter(X_train[:,i],y_pred,color=dlc["dlorange"], label = 'predict')
+ax[0].set_ylabel("Price"); ax[0].legend();
+fig.suptitle("target versus prediction using z-score normalized model")
+plt.show()
